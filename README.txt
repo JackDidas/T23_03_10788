e3c809ba1b25c88b9d6f6507d1180e325fd36e0a
JackDidas <jacksondidas2004@gmail.com>
2025-05-16 02:42:38 +0300

    Portfolio Assignment

---
+++ README.txt
@@ -0,0 +1,36 @@
+Project: Personal Portfolio Website
+
+Tools Used
+-HTML5
+-CSS3
+-JavaScript
+-Google Maps (iframe)
+-Visual Studio Code
+
+Key Features
+-Dark and light theme switch
+-Read more / Read less text toggle
+-Show/hide project details
+-Google Map on contact page
+-Image map with clickable areas
+-Mobile-friendly design
+
+Challenges & Solutions
+-Dark text not visible in dark mode
+-Fixed by using CSS variables for colors
+
+-Map not loading properly
+Used correct embed code from Google Maps
+
+-Text toggle not working
+Fixed by using simple JavaScript to show/hide content
+
+-Layout broken on small screens
+Solved with responsive CSS (flexbox and media queries)
+
+Summary
+This project helped me practice web design using HTML, CSS, and JavaScript. It shows my skills and gives users an easy way to view my work and contact me.
+
+Live Link from GitHub
+
+
--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function greetUser() {
-    const hour = new Date().getHours();
-    let greeting = "Hello!";
-    if (hour < 12) greeting = "Good Morning!";
-    else if (hour < 18) greeting = "Good Afternoon!";
-    else greeting = "Good Evening!";
-    alert(greeting + " Feel free to contact me.");
+  const hour = new Date().getHours();
+  let greeting = "Hello!";
+  if (hour < 12) greeting = "Good Morning!";
+  else if (hour < 18) greeting = "Good Afternoon!";
+  else greeting = "Good Evening!";
+  alert(greeting + " Feel free to contact me.");
+}
+
+function validateContactForm() {
+  const phone = document.getElementById("phone").value;
+  const phoneRegex = /^[0-9]{10}$/;
+
+  if (!phoneRegex.test(phone)) {
+    alert("Please enter a valid 10-digit phone number.");
+    return false;
   }
-  
-  function validateContactForm() {
-    const phone = document.getElementById("phone").value;
-    const phoneRegex = /^[0-9]{10}$/;
-  
-    if (!phoneRegex.test(phone)) {
-      alert("Please enter a valid 10-digit phone number.");
-      return false;
-    }
-  
-    return true;
-  }
-  
+
+  return true;
+}
